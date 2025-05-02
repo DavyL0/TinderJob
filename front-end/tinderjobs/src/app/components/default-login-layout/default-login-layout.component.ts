d8fe37d376f3b85a7641fdad72721b628a05fcfd
Davy Lopes <125506032+DavyL0@users.noreply.github.com>
2025-05-02 00:11:43 -0300

    feat:create login frontend login system

--- front-end/tinderjobs/src/app/components/default-login-layout/default-login-layout.component.ts
+++ front-end/tinderjobs/src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,9 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-default-login-layout',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './default-login-layout.component.html',
   styleUrl: './default-login-layout.component.scss'
 })
@@ -11,4 +16,15 @@ export class DefaultLoginLayoutComponent {
   @Input() title: string = "";
   @Input() primaryBtnText: string = "";
   @Input() secondaryBtnText: string = "";
+  @Output("submit") onSubmit = new EventEmitter();
+  @Output("navigate") onNavigate = new EventEmitter();
+  @Input() disablePrimaryBtn: boolean = true;
+
+  submit(){
+    this.onSubmit.emit();
+  }
+
+  navigate(){
+    this.onNavigate.emit()
+  }
 }
