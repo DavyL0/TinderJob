d9c5668ea9cd61753584fee01b590a1c559f58ce
Davy Lopes <125506032+DavyL0@users.noreply.github.com>
2025-05-04 00:41:35 -0300

    front-end parte users feita

--- front-end/tinderjobs/src/app/pages/user-page/user-page.component.ts
+++ front-end/tinderjobs/src/app/pages/user-page/user-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { VagasUsuarioComponent } from '../../components/vagas-usuario/vagas-usuario.component';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-user-page',
   standalone: true,
-  imports: [VagasUsuarioComponent, CommonModule],
+  imports: [VagasUsuarioComponent,HeaderComponent, CommonModule],
   templateUrl: './user-page.component.html',
   styleUrl: './user-page.component.scss'
 })
