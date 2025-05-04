ce35a7545a3c5fc2e852a8a24149b440c10d9097
Davy Lopes <125506032+DavyL0@users.noreply.github.com>
2025-05-04 16:50:22 -0300

    front-end pagina admin finalizado

--- front-end/tinderjobs/src/app/pages/admin-page/admin-page.component.ts
+++ front-end/tinderjobs/src/app/pages/admin-page/admin-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { VagasComponent } from '../../components/vagas/vagas.component';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-admin-page',
   standalone: true,
-  imports: [VagasComponent, CommonModule],
+  imports: [VagasComponent,HeaderComponent,CommonModule],
   templateUrl: './admin-page.component.html',
   styleUrl: './admin-page.component.scss'
 })
