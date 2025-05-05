6c11a3d36518dfaf78cad554c91d7af0b7a137f5
Davy Lopes <125506032+DavyL0@users.noreply.github.com>
2025-05-04 22:24:14 -0300

    projeto versão final

--- front-end/tinderjobs/src/app/pages/admin-page/admin-page.component.ts
+++ front-end/tinderjobs/src/app/pages/admin-page/admin-page.component.ts
@@ -3,11 +3,12 @@ import { VagasComponent } from '../../components/vagas/vagas.component';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../components/header/header.component';
 import { CandidatosVagaComponent } from '../../components/candidatos-vaga/candidatos-vaga.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-admin-page',
   standalone: true,
-  imports: [VagasComponent,HeaderComponent,CommonModule, CandidatosVagaComponent],
+  imports: [VagasComponent,HeaderComponent,CommonModule, CandidatosVagaComponent,FooterComponent],
   templateUrl: './admin-page.component.html',
   styleUrl: './admin-page.component.scss'
 })
--- front-end/tinderjobs/src/app/pages/user-page/user-page.component.ts
+++ front-end/tinderjobs/src/app/pages/user-page/user-page.component.ts
@@ -3,11 +3,12 @@ import { VagasUsuarioComponent } from '../../components/vagas-usuario/vagas-usua
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../components/header/header.component';
 import { UserInfoComponent } from "../../components/user-info/user-info.component";
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-user-page',
   standalone: true,
-  imports: [VagasUsuarioComponent, HeaderComponent, CommonModule, UserInfoComponent],
+  imports: [VagasUsuarioComponent, HeaderComponent, CommonModule, UserInfoComponent, FooterComponent],
   templateUrl: './user-page.component.html',
   styleUrl: './user-page.component.scss'
 })
